173b8dcc58909acf41a8cf4563a91637aa4b84fd
rothtom <t.roth1@outlook.com>
2024-03-14 21:55:30 +0100

    added some explanation in the url files and implemented a hello world website in the homepage app

--- erfolgsgehirn/erfolgsgehirn/urls.py
+++ erfolgsgehirn/erfolgsgehirn/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path in project includes apps by including their url files and mapping them to whatever you want to name it
+    # path(actual path on website, the urlfile of the app(s) you want to include)
+    path("homepage/", include("homepage.urls")),
+    path('admin/', admin.site.urls)
 ]
