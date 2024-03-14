323ebd0468d28714bd1876f2b123d39fb6251228
rothtom <t.roth1@outlook.com>
2024-03-14 21:59:50 +0100

    removed homepage app as it moved to ardamussbesserennamenmachen

--- erfolgsgehirn/homepage/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    #path(path on the website, view,function that renders the template and does serverside stuff, app_name.function_name)
-    path("", views.index, name="homepage.index")
-]
